1098274b2d6707cf871b1cfdda926ac73b0bc6cd
kenfo1 <kenneth.forsstrom@hotmail.com>
2015-10-21 14:12:44 +0200

    Enum for VehicleType

---
+++ garaget_2/Migrations/201510211149284_enum vehicletypes.cs
@@ -0,0 +1,18 @@
+namespace garaget_2.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class enumvehicletypes : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Vehicles", "VehicleType", c => c.Int(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Vehicles", "VehicleType", c => c.String());
+        }
+    }
+}
--- garaget_2/Migrations/Configuration.cs
+++ garaget_2/Migrations/Configuration.cs
@@ -1,26 +1,23 @@
-namespace garaget_2.Migrations
-{
+namespace garaget_2.Migrations {
     using garaget_2.Models;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<garaget_2.DataAccessLayer.VehicleContext>
-    {
-                public Configuration()
-        {
+    internal sealed class Configuration : DbMigrationsConfiguration<garaget_2.DataAccessLayer.VehicleContext> {
+        public Configuration() {
             AutomaticMigrationsEnabled = true;
         }
 
-        protected override void Seed(garaget_2.DataAccessLayer.VehicleContext context)
-        {
+        protected override void Seed(garaget_2.DataAccessLayer.VehicleContext context) {
             context.Vehicles.AddOrUpdate(v => v.RegNR,
-                new Vehicle { RegNR = "ABC123", VehicleType = "Bil", Color = "Blå", 
-                    Brand = "Opel", Model = "Record", NRofWheels = 4 },
-                new Vehicle { RegNR = "QRC123", VehicleType = "Bil", Color = "Gul", 
-                    Brand = "VW", Model = "1303s", NRofWheels = 4 });        
+                new Vehicle { RegNR = "ABC123", VehicleType = VehicleTypeList.Bil, Color = "Blå", Brand = "Opel", Model = "Record", NRofWheels = 4 },
+                new Vehicle { RegNR = "Gretas", VehicleType = VehicleTypeList.MC, Color = "Flammig", Brand = "HD", Model = "Custom", NRofWheels = 2 },
+                new Vehicle { RegNR = "FGD234", VehicleType = VehicleTypeList.MC, Color = "Svart", Brand = "Husqvarna", Model = "Militär60", NRofWheels = 3 },
+                new Vehicle { RegNR = "QRC123", VehicleType = VehicleTypeList.Bil, Color = "Gul", Brand = "VW", Model = "1303s", NRofWheels = 4 }
+                );
         }
-    
+
     }
 }
