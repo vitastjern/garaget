2e0a8e6d504de692f67a69fee8c02c98843cd75c
Anna Eklund <vitastjern@gmail.com>
2015-10-21 09:53:25 +0200

    Scaffolding for vehicles

--- garaget_2/Migrations/Configuration.cs
+++ garaget_2/Migrations/Configuration.cs
@@ -1,5 +1,6 @@
 namespace garaget_2.Migrations
 {
+    using garaget_2.Models;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
@@ -7,25 +8,19 @@ namespace garaget_2.Migrations
 
     internal sealed class Configuration : DbMigrationsConfiguration<garaget_2.DataAccessLayer.VehicleContext>
     {
-        public Configuration()
+                public Configuration()
         {
             AutomaticMigrationsEnabled = true;
         }
 
         protected override void Seed(garaget_2.DataAccessLayer.VehicleContext context)
         {
-            //  This method will be called after migrating to the latest version.
-
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+            context.Vehicles.AddOrUpdate(v => v.RegNR,
+                new Vehicle { RegNR = "ABC123", VehicleType = "Bil", Color = "Blå", 
+                    Brand = "Opel", Model = "Record", NRofWheels = 4 },
+                new Vehicle { RegNR = "QRC123", VehicleType = "Bil", Color = "Gul", 
+                    Brand = "VW", Model = "1303s", NRofWheels = 4 });        
         }
+    
     }
 }
