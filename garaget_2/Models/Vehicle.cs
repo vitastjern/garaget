45d09ebf76697f3a866c68e866767fb223501076
kenfo1 <kenneth.forsstrom@hotmail.com>
2015-10-22 10:23:37 +0200

    Validation & errormessages

--- garaget_2/Models/Vehicle.cs
+++ garaget_2/Models/Vehicle.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Web;
 
 namespace garaget_2.Models {
-    public enum VehicleTypeList { Cykel, MC, Bil, SUV, Limousine, Bil_m_släp, Lastbil, Buss }
+    public enum VehicleTypeList { Bil, SUV, Limousine, MC, Cykel, Bil_m_släp, Lastbil, Buss, Annan }
    
     public class Vehicle {
         public int Id { get; set; }
@@ -14,18 +14,28 @@ namespace garaget_2.Models {
         public VehicleTypeList VehicleType { get; set; }
 
         [Display(Name = "Reg.identitet")]
+        [Required(ErrorMessage="Fråga efter legitimation, istället?")]
+        [MinLength(4,ErrorMessage=("Enlig Wiki kan man inte identifiera det ni skrev!"))]
+        [MaxLength(32, ErrorMessage = "Det här är inte rätt ställe att skriva noveller!")]
         public string RegNR { get; set; }
 
         [Display(Name="Färg")]
+        [MinLength(3, ErrorMessage="Vit, röd, blå, gul, blå & grå är korta färgnamn")]
+        [MaxLength(32, ErrorMessage="Det här är inte rätt ställe att skriva noveller!")]
         public string Color { get; set; }
 
         [Display(Name="Märke")]
+        [MinLength(3, ErrorMessage = "Om ni anger något, skriv vettigt!")]
+        [MaxLength(32, ErrorMessage = "Det här är inte rätt ställe att skriva noveller!")]
         public string Brand { get; set; }
         
         [Display(Name="Modell")]
+        [MinLength(2,ErrorMessage="I denna värld finns inte så korta modellnamn")]
+        [MaxLength(32, ErrorMessage = "Det här är inte rätt ställe att skriva noveller!")]
         public string Model { get; set; }
 
         [Display(Name="Antal hjul")]
+        [Range(1,22,ErrorMessage="Stridsvagnar och snöskotrar skadar golvet, hjul krävs!")]
         public int NRofWheels { get; set; }
 
         [Display(Name = "Tidpunkt för incheckning")]
