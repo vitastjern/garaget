4c0595459d4d99ae26d9674492541f2eb284320b
Anna Eklund <vitastjern@gmail.com>
2015-10-29 15:52:34 +0100

    Skapat medlemsvyer och knutit dem till checkin/checkout av fordon

--- garaget_2/Models/VehicleType.cs
+++ garaget_2/Models/VehicleType.cs
@@ -8,6 +8,8 @@ namespace garaget_2.Models {
     public class VehicleType {
         [Key]
         public int VehicleTypeId { get; set; }
+
+        [Display(Name="Fordonstyp")]
         public string VehicleTypeName { get; set; }
     }
 }
